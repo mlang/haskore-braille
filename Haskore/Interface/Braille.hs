08a03f28f96008766a7e7e300d748c431bfa4c0f
Mario Lang <mlang@delysid.org>
2015-10-02 20:21:46 +0200

    Use Haskore.Basic.Duration.T and refactor.

--- Haskore/Interface/Braille.hs
+++ Haskore/Interface/Braille.hs
@@ -5,18 +5,20 @@ module Haskore.Interface.Braille (
 
 import Control.Applicative (many, some, (<$>), (<*>), (*>), (<|>))
 import Control.Monad (guard)
-import Control.Monad.Trans.State (get, put, runStateT)
+import Control.Monad.Trans.State (get, gets, put, runStateT)
 import Data.Bits ((.&.))
 import Data.Functor (($>))
 import Data.Monoid (mappend)
 import Data.Traversable (traverse)
 import qualified Haskore.Basic.Pitch as Pitch (Class(..), transpose)
+import qualified Haskore.Basic.Duration as Duration (T)
+import Haskore.Basic.Duration as Duration ((%+))
 import Text.Parsec (lookAhead, parse, satisfy, sepBy, try, (<?>))
 import Text.Parsec.Combinator (choice)
 import Text.Parsec.String (Parser)
 
 -- Braille music code only uses the old 6-dot system.  We enumerate all
--- possible combinations of dots to use the type system to avoid accidentally
+-- possible dot patterns to use the type system to avoid accidentally
 -- specifying invalid dot patterns in the source code.
 --
 -- genDataBraille :: String
@@ -26,7 +28,7 @@ import Text.Parsec.String (Parser)
 --     ctorName :: Int -> String
 --     ctorName = (++) "Dot" . concatMap (show . succ) . flip filter [0..5] . testBit
 
-data Braille = NoDots | Dot1 | Dot2 | Dot12 | Dot3 | Dot13 | Dot23 | Dot123 | Dot4
+data SixDots = NoDots | Dot1 | Dot2 | Dot12 | Dot3 | Dot13 | Dot23 | Dot123 | Dot4
              | Dot14 | Dot24 | Dot124 | Dot34 | Dot134 | Dot234 | Dot1234 | Dot5
              | Dot15 | Dot25 | Dot125 | Dot35 | Dot135 | Dot235 | Dot1235 | Dot45
              | Dot145 | Dot245 | Dot1245 | Dot345 | Dot1345 | Dot2345 | Dot12345
@@ -38,15 +40,15 @@ data Braille = NoDots | Dot1 | Dot2 | Dot12 | Dot3 | Dot13 | Dot23 | Dot123 | Do
              deriving (Bounded, Enum, Eq, Read, Show)
 
 -- | Convert to Unicode Braille.
-toChar :: Braille -> Char
+toChar :: SixDots -> Char
 toChar = toEnum . (+ 0x2800) . fromEnum
 
 -- | Match a single Braille cell.
-brl :: Braille -> Parser Braille
+brl :: SixDots -> Parser SixDots
 brl b = satisfy (== toChar b) $> b <?> [toChar b]
 
 -- | Matches any Unicode Braille character.
-anyBrl :: Parser Braille
+anyBrl :: Parser SixDots
 anyBrl = toBraille <$> satisfy (isInUBrlBlock . fromEnum) where
   toBraille = toEnum . flip (-) 0x2800 . fromEnum
   isInUBrlBlock c = c >= 0x2800 && c <= 0x28FF
@@ -60,7 +62,7 @@ augmentationDotsP = length <$> many (brl Dot3)
 -- Braille music is inherently ambiguous.  The time signature is necessary
 -- to automatically calculate the real values of notes and rests.
 data AmbiguousValue = WholeOr16th | HalfOr32th | QuarterOr64th | EighthOr128th
-                    deriving (Enum, Eq, Show)
+                    deriving (Enum, Eq, Read, Show)
 
 -- | A Braille music symbol.
 data AmbiguousSign =
@@ -106,7 +108,7 @@ rest = AmbiguousRest <$> ambiguousValueP <*> augmentationDotsP where
                            , brl Dot1346 $> EighthOr128th
                            ]
 
--- A Braille music measure can contain parallel and sequential music.
+-- A Braille music measure can contain parallel and serial music.
 -- The inner most voice is called partial voice because it can potentially
 -- span just across a part of the whole measure.
 type AmbiguousPartialVoice = [AmbiguousSign]
@@ -120,7 +122,7 @@ type AmbiguousPartialMeasure = [AmbiguousPartialVoice]
 partialMeasureP :: Parser AmbiguousPartialMeasure
 partialMeasureP = sepBy partialVoiceP $ brl Dot5 *> brl Dot2
 
--- | A voice consists of one or more partial measures.
+-- | A voice consists of one or more serial partial measures.
 type AmbiguousVoice = [AmbiguousPartialMeasure]
 
 voiceP :: Parser AmbiguousVoice
@@ -135,81 +137,58 @@ measureP = sepBy voiceP $ brl Dot126 *> brl Dot345
 -- With the basic data structure defined and parsed into, we can finally
 -- move towards the actually interesting task of disambiguating values.
 
-data Sign =
-     Note { originalValue :: AmbiguousValue
-          , realValue :: Rational
-          , step :: Pitch.Class
-          , augmentationDots :: AugmentationDots
-          }
-   | Rest { originalValue :: AmbiguousValue
-          , realValue :: Rational
-          , augmentationDots :: AugmentationDots
-          }
+data Sign = Note { realValue :: Duration.T, ambiguous :: AmbiguousSign }
+          | Rest { realValue :: Duration.T, ambiguous :: AmbiguousSign }
           -- more to be added (Chord, ...)
           deriving (Eq, Show)
 
-mkSign :: Rational -> AmbiguousSign -> Sign
-mkSign rv (AmbiguousNote { ambiguousValue = v
-                         , ambiguousStep = s
-                         , ambiguousAugmentationDots = d
-                         }) =
-  Note { realValue = rv
-       , originalValue = v
-       , step = s
-       , augmentationDots = d
-       }
-mkSign rv (AmbiguousRest { ambiguousValue = v
-                         , ambiguousAugmentationDots = d
-                         }) =
-  Rest { realValue = rv
-       , originalValue = v
-       , augmentationDots = d
-       }
+mkSign :: Duration.T -> AmbiguousSign -> Sign
+mkSign v n@(AmbiguousNote {}) = Note v n
+mkSign v r@(AmbiguousRest {}) = Rest v r
 
-data PartialVoice = PartialVoice Rational [Sign]
+data PartialVoice = PartialVoice Duration.T [Sign]
 type PartialMeasure = [PartialVoice]
 type Voice = [PartialMeasure]
 type Measure = [Voice]
 
 -- | Given a maximum time, return all possible interpretations
 -- of the given PartialVoice.
-pvs :: Rational -> AmbiguousPartialVoice -> [PartialVoice]
+pvs :: Duration.T -> AmbiguousPartialVoice -> [PartialVoice]
 pvs = curry $ map (mkPV . fst) . runStateT (allWhich (large <|> small)) where
   allWhich p = do a <- p
-                  (l,xs) <- get
-                  guard (l >= 0)
-                  if not $ null xs then (mappend a) <$> allWhich p else return a
+                  xs <- gets snd
+                  if not $ null xs then mappend a <$> allWhich p else return a
   large  = one 0
   small  = one 4
   -- ... Move rules to come which will eventually return lists with length > 1.
   one o  = do (l, x:xs) <- get
               let v = 2 ^^ (-(o + fromEnum (ambiguousValue x)))
               let sign = mkSign v x
+              guard (l >= dur sign)
               put (l - dur sign, xs)
               return [sign]
   mkPV signs = PartialVoice (sum $ map dur signs) signs
 
-pms :: Rational -> AmbiguousPartialMeasure -> [PartialMeasure]
+pms :: Duration.T -> AmbiguousPartialMeasure -> [PartialMeasure]
 pms l = filter allEqDur . traverse (pvs l)
 
-vs :: Rational -> AmbiguousVoice -> [Voice]
+vs :: Duration.T -> AmbiguousVoice -> [Voice]
 vs _ []     = return []
-vs l (x:xs) = do pm <- pms l x
-                 (pm :) <$> vs (l - dur pm) xs
+vs l (x:xs) = pms l x >>= \pm -> (pm :) <$> vs (l - dur pm) xs
 
 -- | Given the current time signature (meter), return a list of all possible
 -- interpretations of the given measure.
-ms :: Rational -> AmbiguousMeasure -> [Measure]
+ms :: Duration.T -> AmbiguousMeasure -> [Measure]
 ms l = filter allEqDur . traverse (vs l)
 
--- Apparently inefficient helper.
-allEqDur :: Duration a => [a] -> Bool
+allEqDur :: HasDuration a => [a] -> Bool
 allEqDur xs = all ((== dur (head xs)) . dur) (tail xs)
 
 -- | Test measure disambiguation.
 testms l s = ms l <$> parse measureP "" s
 
-augmentationDotsFactor dots = pred (2 ^ succ dots) / (2 ^ dots)
+augmentationDotsFactor :: Integral a => a -> Duration.T
+augmentationDotsFactor dots = pred (2 ^ succ dots) %+ (2 ^ dots)
 
 -- | A well-known time-consuming test case from Bach's Goldberg Variation #3.
 -- In general, music with meter > 1 is more time-consuming because
@@ -219,21 +198,27 @@ test = let l = 3/2 in
        do candidates <- testms l "⠺⠓⠳⠛⠭⠭⠚⠪⠑⠣⠜⠭⠵⠽⠾⠮⠚⠽⠾⠮⠾⠓⠋⠑⠙⠛⠊"
           return $ length $ filter (== l) $ map dur candidates
 
-class Duration a where
-  dur :: a -> Rational
+class HasDuration a where
+  dur :: a -> Duration.T
 
-instance Duration Sign where
+instance HasDuration Sign where
+  -- | The duration of a disambiguated sign is the product of its value,
+  -- the augmentation dots factor, and (unimplemented) stretch factor from tuplets
+  -- or multi meter parallel staves.
   dur = product . flip map factors . flip ($) where
-    factors = [realValue, augmentationDotsFactor . augmentationDots]
+    factors = [ realValue
+              , augmentationDotsFactor . ambiguousAugmentationDots . ambiguous
+              -- tuplet factor here
+              ]
 
-instance Duration PartialVoice where
-  dur (PartialVoice d _) = d
+instance HasDuration PartialVoice where
+  dur (PartialVoice d _) = d -- avoid recomputing duration
 
-instance Duration PartialMeasure where
+instance HasDuration PartialMeasure where
   dur = dur . head
 
-instance Duration Voice where
+instance HasDuration Voice where
   dur = sum . map dur
 
-instance Duration Measure where
+instance HasDuration Measure where
   dur = dur . head
